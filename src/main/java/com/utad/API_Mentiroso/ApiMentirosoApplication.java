64fca3548d558cbb8fff1c9701ed5c1e4162b717
pc <jorgeav2212@gmail.com>
2025-12-16 11:37:48 +0100

    Commit 1 jorge

---
+++ src/main/java/com/utad/API_Mentiroso/ApiMentirosoApplication.java
@@ -0,0 +1,13 @@
+package com.utad.API_Mentiroso;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiMentirosoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiMentirosoApplication.class, args);
+	}
+
+}
