6c9c24884cbd20b0a39c91837c4cbfdcb107d25b
Pablo Sainero Valle <pablo.sainero@live.u-tad.com>
2025-12-16 11:50:48 +0100

    Commit getter y setters jugada

--- src/main/java/com/utad/API_Mentiroso/Jugada.java
+++ src/main/java/com/utad/API_Mentiroso/Jugada.java
@@ -1,5 +1,55 @@
 package com.utad.API_Mentiroso;
 
+import java.util.ArrayList;
+
 public class Jugada {
+	private boolean esVerdad;
+	private ArrayList<Integer> cartasJugadas;
+	private String nombre;
+	private int primerNumero;
+	private int segundoNumero;
+
+	public Jugada() {
+	}
+
+	public boolean isEsVerdad() {
+		return esVerdad;
+	}
+
+	public void setEsVerdad(boolean esVerdad) {
+		this.esVerdad = esVerdad;
+	}
+
+	public ArrayList<Integer> getCartasJugadas() {
+		return cartasJugadas;
+	}
+
+	public void setCartasJugadas(ArrayList<Integer> cartasJugadas) {
+		this.cartasJugadas = cartasJugadas;
+	}
+
+	public String getNombre() {
+		return nombre;
+	}
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+
+	public int getPrimerNumero() {
+		return primerNumero;
+	}
+
+	public void setPrimerNumero(int primerNumero) {
+		this.primerNumero = primerNumero;
+	}
+
+	public int getSegundoNumero() {
+		return segundoNumero;
+	}
+
+	public void setSegundoNumero(int segundoNumero) {
+		this.segundoNumero = segundoNumero;
+	}
 
 }
