df981ca70186778ab1f5cb1fe975c0a0b2b10092
Fernando García <garcia.fernandomg@gmail.com>
2025-12-30 19:10:31 +0100

    Commit endpoint 1 y 2

--- src/main/java/com/utad/API_Mentiroso/ApiMentirosoApplication.java
+++ src/main/java/com/utad/API_Mentiroso/ApiMentirosoApplication.java
@@ -5,6 +5,7 @@ import java.util.UUID;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -13,7 +14,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class ApiMentirosoApplication {
 	UUID idSala = UUID.randomUUID();
 	static HashMap<UUID, Partida> partidas;
-	//String idStr = idSala.toString();
+	// String idStr = idSala.toString();
 
 	public static void main(String[] args) {
 		partidas = new HashMap<>();
@@ -21,23 +22,80 @@ public class ApiMentirosoApplication {
 		SpringApplication.run(ApiMentirosoApplication.class, args);
 	}
 
+	// Endpoint 1
+	@GetMapping("/crear")
+	public HashMap<String, Object> crearPartida(@RequestParam(value = "username") String username) {
+
+		// Futura respuesta a devolver
+		HashMap<String, Object> response = new HashMap<>();
+		// Crear partida
+		UUID salaID = UUID.randomUUID();
+		Partida partida = new Partida(salaID);
+		// Crear jugador
+		Jugador jugador = new Jugador(username);
+
+		// Repartir mano
+		jugador.setMano(partida.darMano());
+
+		// Añadir jugador a la partida
+		partida.getJugadores().add(jugador);
+
+		// Guardar partida
+		partidas.put(salaID, partida);
+
+		// Respuesta
+		response.put("salaID", salaID.toString());
+		response.put("mano", jugador.getMano());
+
+		return response;
+	}
+
+	// Endpoint 2
+	@GetMapping("/unirse")
+	public HashMap<String, Object> unirsePartida(@RequestParam(value = "salaID") String salaID,
+			@RequestParam(value = "username") String username) {
+
+		// Futura respuesta a devolver
+		HashMap<String, Object> response = new HashMap<>();
+
+		UUID id = UUID.fromString(salaID);
+		Partida partida = partidas.get(id);
+
+		// No existe la partida
+		if (partida == null || !partida.estaLlena()) {
+			response.put("ok", false);
+			return response;
+		}
+
+		// Crear jugador
+		Jugador jugador = new Jugador(username);
+		jugador.setMano(partida.darMano());
+
+		// Añadir a la partida
+		partida.getJugadores().add(jugador);
+
+		response.put("ok", true);
+		response.put("mano", jugador.getMano());
+
+		return response;
+	}
+
 	/**
-	 * Endpoint 3: anterior jugada
-	 * Recibe...
-	 * - el nombre del jugador que la está llamando
-	 * - ID de la partida
-	 * Envía la jugada anterior (t)
+	 * Endpoint 3: anterior jugada Recibe... - el nombre del jugador que la está
+	 * llamando - ID de la partida Envía la jugada anterior (t)
+	 * 
 	 * @return
 	 */
-	public Jugada JugadaAnterior (@RequestParam(value = "gameID", defaultValue="") String gameID, @RequestParam(value = "username", defaultValue="") String name) {
-		//Localizar la id del juego mediante su ID
+	public Jugada JugadaAnterior(@RequestParam(value = "gameID", defaultValue = "") String gameID,
+			@RequestParam(value = "username", defaultValue = "") String name) {
+		// Localizar la id del juego mediante su ID
 		Partida myGame = partidas.get(UUID.fromString(name));
 
-		//Encontrar al jugador anterior al dado
+		// Encontrar al jugador anterior al dado
 
-		//Localizar a su anterior
+		// Localizar a su anterior
 
-		//devolvemos su jugada anterior
-		return ;
+		// devolvemos su jugada anterior
+		return;
 	}
 }
